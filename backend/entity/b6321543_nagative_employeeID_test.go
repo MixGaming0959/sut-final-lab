41ce9719591bacbfb81a6cca0caf6ae4c33b6c1f
unknown <wasutew@gmail.com>
2023-02-15 14:03:16 -0800

    Negative EmployeeID Test - close #4

---
+++ backend/entity/b6321543_nagative_employeeID_test.go
@@ -0,0 +1,26 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/asaskevich/govalidator"
+	. "github.com/onsi/gomega"
+)
+
+func Test_NegativeEmployeeIDTest(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	emp := Employee{
+		Name:       "นายวสุธา เศรษฐบุตร",
+		Email:      "wasuta@gmail.com",
+		EmployeeID: "B12345678",
+	}
+
+	ok, err := govalidator.ValidateStruct(emp)
+
+	g.Expect(ok).ToNot(BeTrue())
+
+	g.Expect(err).ToNot(BeNil())
+
+	g.Expect(err.Error()).To(Equal("รหัสพนักงานขึนต้นด้วย J หรือ M หรือ S แล้วตามด้วยตัวเลขจํานวน 8 ตัว"))
+}
